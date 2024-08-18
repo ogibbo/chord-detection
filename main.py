62ba57277ca05db98c2da272f9e5723392d7bf37
Oliver <olivergibbonsuk@gmail.com>
2024-08-18 13:54:41 +0100

    collected raw dataset for 4 chords, combined into one pickle

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import cv2
+import numpy as np
+import torch
+import pickle
 
 from hand_tracking import (
     VideoStream,
     HandTracker,
     DrawingUtils,
 )
+from utils import get_hand_tensor
 
 def main():
+
+    # To do: Figure out better way to handle this
+    GET_TRAINING_DATA = True
+    CHORD = "C"
+    COLLECTED_DATA = []
+    N = 1000
 
     # Initialising video stream, source may be different for you
     video_stream = VideoStream(source=0)
@@ -26,6 +36,19 @@ def main():
                 drawing_utils.draw_landmarks(
                     bgr_frame, hand_landmarks, hand_tracker.mp_hands.HAND_CONNECTIONS
                 )
+
+                ######### Add functionality to get training data #########
+                # Get the landmarks of the hand and save them as a 
+                hand_tensor = get_hand_tensor(hand_landmarks)
+
+                COLLECTED_DATA.append((hand_tensor, CHORD))
+                video_stream.wait_key(10)
+                print(len(COLLECTED_DATA))
+                if len(COLLECTED_DATA) == N:
+                    with open(f'data/tensors_{CHORD}.pkl', 'wb') as handle:
+                        pickle.dump(COLLECTED_DATA, handle)
+                    break
+
 
         video_stream.show_frame("Hand Tracking", bgr_frame)
         
---
+++ utils/utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+import torch
+
+def get_hand_tensor(hand_landmarks):
+
+    landmark_list = []
+                    
+    for landmark in hand_landmarks.landmark:
+        
+        landmark_list.append([landmark.x, landmark.y, landmark.z])
+        landmark_array = np.array(landmark_list)
+        landmark_tensor = torch.tensor(landmark_array, dtype=torch.float32)\
+        
+    return landmark_tensor
